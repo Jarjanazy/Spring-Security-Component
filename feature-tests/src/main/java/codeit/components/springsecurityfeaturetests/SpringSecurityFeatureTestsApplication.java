24e4e51cdd4522ea6c463ed410103bb1ebf206a9
Jarjanazy <celil.cercenazi@gmail.com>
2022-06-26 20:05:34 +0200

    add spring-security-feature-tests empty maven module

---
+++ feature-tests/src/main/java/codeit/components/springsecurityfeaturetests/SpringSecurityFeatureTestsApplication.java
@@ -0,0 +1,13 @@
+package codeit.components.springsecurityfeaturetests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFeatureTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFeatureTestsApplication.class, args);
+	}
+
+}
---
+++ feature-tests/src/test/java/codeit/components/springsecurityfeaturetests/SpringSecurityFeatureTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package codeit.components.springsecurityfeaturetests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFeatureTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
